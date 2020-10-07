d4ff678050a13001333d760625722cabacedfcf4
Lesnov Artirm <russskey@gmail.com>
2020-10-07 16:30:17 +0300

    prettier

--- src/taxi_app/Home.jsx
+++ src/taxi_app/Home.jsx
@@ -1,12 +1,21 @@
-import React from "react"
+import React from 'react';
 
-export const Home = () => {
+export const Home = (props) => {
+	const onClick = (e) => {
+		e.preventDefault();
+		props.setPage("map")
+	}
+
 	return (
-		<form>
-			<label htmlFor="email">Email:</label>
-			<input id="email" type="email" name="email" size="28"/>
-			<label htmlFor="password">Password:</label>
-			<input id="password" type="password" name="password" size="28"/>
-		</form>
+
+		<div className="login-wrapper">
+			<form className="login-wrapper__form">
+				<label htmlFor="email">Email:</label>
+				<input className="login-wrapper__input" id="email" type="email" name="email" size="28"/>
+				<label htmlFor="password">Password:</label>
+				<input className="login-wrapper__input" id="password" type="password" name="password" size="28"/>
+				<button onClick={onClick} className="login-wrapper__btn btn">LogIn</button>
+			</form>
+		</div>
 	)
 }
---
+++ src/taxi_app/Registration.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+export const Registration = (props) => {
+	
+	const toMap = (e) => {
+		e.preventDefault();
+		props.setPage("map")
+	}
+	const toHome = (e) => {
+		props.setPage("home")
+	}
+	return (
+		<div className="register-wrapper">
+			<h1 className="register-wrapper__title">Registration</h1>
+			<p>If You already have it - <a href="#login" className="register-wrapper__link" onClick={toHome}>WELLCOME</a></p>
+			<form>
+				<label htmlFor="email">Email address</label>
+				<input className="register-wrapper__input" type="text" name="email"></input>
+				<div className="register-wrapper__full-name">
+					<div className="two-columns">
+						<label htmlFor="name" className="label-fullname">Name</label>
+						<input className="two-columns-input" type="text" name="name"></input>
+					</div>
+					<div className="two-columns">
+						<label htmlFor="lastName" className="label-fullname">Last Name</label>
+						<input className="two-columns-input" type="text" name="lastName"></input>
+					</div>
+				</div>
+				<label htmlFor="lastName">Password</label>
+				<input className="register-wrapper__input" type="text" name="password"></input>
+			</form>
+			<button onClick={toMap} className="register-wrapper__btn btn">log in</button>
+		</div>
+	);
+}
+
