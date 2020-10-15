3848e6cc6e304006a8566f54a35bc766a3fdc268
Lesnov Artirm <russskey@gmail.com>
2020-10-15 22:31:39 +0300

    map with F-component & C-component

--- src/taxi_app/Map.jsx
+++ src/taxi_app/Map.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 
+// export const Map = () => {
+// 	let mapContainer = React.createRef();
+
+// 	useEffect(() => {
+// 		mapboxgl.accessToken =
+// 			'pk.eyJ1IjoibGVzbm92YXJ0ODgiLCJhIjoiY2tnYWoybzI3MDdrNzJ3bzR3NW8yODI1YiJ9.UQuGd997fXL8TOrL39Rzxw';
+
+// 		const map = new mapboxgl.Map({
+// 			container: mapContainer,
+// 			mapStyle: 'mapbox://styles/mapbox/streets-v9',
+// 			center: [ 30.3056504, 59.9429126 ],
+// 			zoom: 10,
+// 			width: 100,
+// 			height: 100,
+// 		});
+// 	});
+
+// 	return (
+// 		<div className='map-wrapper'>
+// 			<div className='map' ref={(el) => (mapContainer = el)} />
+// 		</div>
+// 	);
+// };
+
 export class Map extends Component {
 	map = null;
 	mapContainer = React.createRef();
@@ -10,7 +34,7 @@ export class Map extends Component {
 			'pk.eyJ1IjoibGVzbm92YXJ0ODgiLCJhIjoiY2tnYWoybzI3MDdrNzJ3bzR3NW8yODI1YiJ9.UQuGd997fXL8TOrL39Rzxw';
 
 		this.map = new mapboxgl.Map({
-			container: this.mapContainer.current,
+			container: this.mapContainer,
 			mapStyle: 'mapbox://styles/mapbox/streets-v9',
 			center: [30.3056504, 59.9429126],
 			zoom: 10,
@@ -25,7 +49,7 @@ export class Map extends Component {
 	render() {
 		return (
 			<div className="map-wrapper">
-				<div className="map" ref={this.mapContainer} />
+				<div className="map" ref={el => this.mapContainer = el} />
 			</div>
 		);
 	}
